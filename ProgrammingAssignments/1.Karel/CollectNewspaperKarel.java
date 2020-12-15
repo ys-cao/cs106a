54680f43245ecb57643241e5ac16a00f95885c47
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2020-12-14 21:10:59 -0500

    Create CollectNewspaperKarel.java

---
+++ ProgrammingAssignments/1.Karel/CollectNewspaperKarel.java
@@ -0,0 +1,50 @@
+* File: CollectNewspaperKarel.java
+ * --------------------------------
+ * At present, the CollectNewspaperKarel subclass does nothing.
+ * Your job in the assignment is to add the necessary code to
+ * instruct Karel to walk to the door of its house, pick up the
+ * newspaper (represented by a beeper, of course), and then return
+ * to its initial position in the upper left corner of the house.
+ */
+
+import stanford.karel.*;
+
+public class CollectNewspaperKarel extends SuperKarel {
+
+	public void run() {
+		moveToNewspaper();
+		pickBeeper();
+		returnToStartPoint();
+	}
+	
+	private void moveToNewspaper() {
+		move();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+	
+	private void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
+	}
+
+	private void returnToStartPoint() {
+		turnAround();
+		move();
+		move();
+		move();
+		turnRight();
+		move();
+		turnRight();
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
+
+}
