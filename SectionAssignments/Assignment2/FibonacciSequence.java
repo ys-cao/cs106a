28db854a5b6bdeb3d096c19e68b0c9170c6ad785
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2020-12-19 18:48:41 -0500

    Update FibonacciSequence.java

--- SectionAssignments/Assignment2/FibonacciSequence.java
+++ SectionAssignments/Assignment2/FibonacciSequence.java
@@ -2,20 +2,21 @@
  * File: FibonacciSequence.java
  * This program is to show the Fibonacci Sequence not to exceed 10,000.
  */
+import acm.program.*;
  
- public void run() {
-     println("This program lists the Fibonacci sequence.");
-     println("0")
-     int i = 0;
-     int j = 1;
-     
-     private static final max = 10000;
-     
-     while (n < max) {
-         println (i);
-         int n = i + j;
-         i = j;
-         j = n;
-     }
- }
+public class Fibonacci extends ConsoleProgram {
+    public void run() {
+        println("This program lists the Fibonacci sequence.");
+        println("0")
+        int i = 0;
+        int j = 1;
+        private static final max = 10000;
+        while (n < max) {
+            println (i);
+            int n = i + j;
+            i = j;
+            j = n;
+        }
+    }
+}
  
