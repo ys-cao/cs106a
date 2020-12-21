6d272582460f91c5377868dccda0382ed18b3ecb
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2020-12-21 17:49:18 -0500

    Create RandomCircles.java

---
+++ SectionAssignments/Assignment3/RandomCircles.java
@@ -0,0 +1,15 @@
+/*
+ * File: RandomCircles.java
+ * ------------------------
+ * Write a GraphicsProgram that draws a set of ten circles with different sizes, positions,
+ * and colors. Each circle should have a randomly chosen color, a randomly chosen radius
+ * between 5 and 50 pixels, and a randomly chosen position on the canvas, subject to the
+ * condition that the entire circle must fit inside the canvas without extending past the edge. 
+ */
+import acm.util.*;
+import acm.graphics.*;
+import acm.program.*;
+
+public class RandomCircles extends GraphicsProgram {
+
+}
