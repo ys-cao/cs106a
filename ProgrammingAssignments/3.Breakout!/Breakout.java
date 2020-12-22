09756d8c16ae409bc26993c4b7566999d0711a82
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2020-12-21 22:46:02 -0500

    Create Breakout.java

---
+++ ProgrammingAssignments/3.Breakout!/Breakout.java
@@ -0,0 +1,30 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import acm.program.GraphicsProgram;
+
+public class Breakout extends GraphicsProgram {
+    public static final int APPLICATION_WIDTH = 400;
+    public static final int APPLICATION_HEIGHT = 600;
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 600;
+    private static final int PADDLE_WIDTH = 60;
+    private static final int PADDLE_HEIGHT = 10;
+    private static final int PADDLE_Y_OFFSET = 30;
+    private static final int NBRICKS_PER_ROW = 10;
+    private static final int NBRICK_ROWS = 10;
+    private static final int BRICK_SEP = 4;
+    private static final int BRICK_WIDTH = 36;
+    private static final int BRICK_HEIGHT = 8;
+    private static final int BALL_RADIUS = 10;
+    private static final int BRICK_Y_OFFSET = 70;
+    private static final int NTURNS = 3;
+
+    public Breakout() {
+    }
+
+    public void run() {
+    }
+}
